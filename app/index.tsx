089119d239a13b61b010545109b2cd208d9ae6df
wale22 <walemustapha08@gmail.com>
2025-06-14 23:06:08 +0100

    first step implementation

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import style from "@/styles/indexStyle"
+
+export default function Index() {
+  return (
+    <View
+      style={style.background}
+    >
+      <Text style={style.text}>home screen.</Text>
+    </View>
+  );
+}
