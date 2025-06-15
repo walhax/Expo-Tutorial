75f177ef5668288b0265ad5a769c3f5f99e20dec
wale22 <walemustapha08@gmail.com>
2025-06-15 23:05:33 +0100

    navigation links added.

--- app/About.tsx
+++ app/About.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import style from '@/styles/AboutStyle'
+import { Link } from 'expo-router'
 
 export default function About() {
   return (
     <View style={style.background}>
-      <Text style={style.text}>This is About screen.</Text>
+      <Text style={style.text}>About Screen.</Text>
+      <Link href={'./'}>Go back to home screen.</Link>
     </View>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from "react-native";
 import style from "@/styles/indexStyle"
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View
       style={style.background}
     >
-      <Text style={style.text}>This is home screen.</Text>
+      <Text style={style.text}>Home Screen.</Text>
+      <Link href={'./About'}>Go to About screen</Link>
     </View>
   );
 }
