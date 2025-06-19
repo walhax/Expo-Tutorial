87bed31bac9e26f7a2c8e05cc3c3a6be9a43cc8b
Leonard Bryan <jackbryan3214@gmail.com>
2025-06-19 17:00:52 +0100

    drag  effect added

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -10,6 +10,9 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
 
   const doubleTap = Gesture.Tap()
   .numberOfTaps(2)
@@ -28,16 +31,37 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   };
   });
 
-  return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </GestureDetector>
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
 
-    </View>
+  const drag = Gesture.Pan().onChange(event => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+  return (
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </GestureDetector>
+
+      </Animated.View>
+    </GestureDetector>
+
   );
 }
