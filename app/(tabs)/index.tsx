6d48baf6b67694b294d7905f6e845deea7c28ac7
Leonard Bryan <jackbryan3214@gmail.com>
2025-06-20 23:39:17 +0100

    screenshot added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import {View,ImageSourcePropType } from "react-native";
+import {View,ImageSourcePropType,Platform } from "react-native";
 import { useRef, useState } from "react";
 import * as MediaLibrary from 'expo-media-library';
 import * as imgPicker from "expo-image-picker"
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
@@ -63,19 +64,36 @@ export default function Index() {
   };
 
 
- const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert('Saved!');
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
@@ -85,7 +103,7 @@ export default function Index() {
     >
       <View style={imgstyles.imageContainer}>
         <View ref={imageRef} collapsable={false}>
-          <ImageViewer imageSource={selectedImage? selectedImage : PlaceholderImage} />
+          <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
           {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
         </View>
     
--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -2,12 +2,13 @@ import { ImageSourcePropType} from 'react-native'
 import { Image } from 'expo-image'
 import styles from '@/styles/image'
 
-type Props={
- imageSource:ImageSourcePropType
-}
+type Props = {
+  imgSource: ImageSourcePropType;
+  selectedImage?: string;
+};
 
-export default function ImageViewer({imageSource}:Props) {
-  return (
-    <Image source={imageSource} style={styles.imageViewer}/>
-  )
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
+  return <Image source={imageSource} style={styles.imageViewer} />;
 }
